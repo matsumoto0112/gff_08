c1ac1a7c2b748968c43bdd9142423481275cc8d2
Dem4236 <onoderadesk0402@gmail.com>
2020-11-13 12:18:40 +0900

    自分がオーナーかどうかを調べる関数の作成

---
+++ gff_08/Source/gff_08/utils/NetworkConnectUtility.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "StrixBlueprintFunctionLibrary.h"
+
+#include "NetworkConnectUtility.generated.h"
+
+/**
+ *
+ */
+UCLASS()
+class GFF_08_API UNetworkConnectUtility : public UStrixBlueprintFunctionLibrary {
+	GENERATED_BODY()
+
+	/**
+	* プレイヤーがオーナー(操作しているもの)かどうか
+	* @param [target] 調べるアクタ
+	* @return オーナーかどうか
+	*/
+	UFUNCTION(BlueprintCallable, Category = "StrixSDKUtility")
+	static bool IsOwner(const AActor* target);
+};
