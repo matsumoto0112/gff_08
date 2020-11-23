e3d12bbb09b8880d11d4be12ab72ba770cf25bba
Dem4236 <onoderadesk0402@gmail.com>
2020-11-23 23:20:19 +0900

    ルーム接続時の処理をC++で記述

--- gff_08/Source/gff_08/network/NetworkConnect.cpp
+++ gff_08/Source/gff_08/network/NetworkConnect.cpp
@@ -7,10 +7,12 @@
 #include <Adapter\StrixPropertyMap.h>
 #include <StrixBlueprintFunctionLibrary.h>
 
+using StrixUtil = UStrixBlueprintFunctionLibrary;
+
 // Sets default values
 ANetworkConnect::ANetworkConnect() {
 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bCanEverTick = false;
 }
 
 // Called when the game starts or when spawned
@@ -26,7 +28,7 @@ void ANetworkConnect::Tick(float DeltaTime) {
 
 bool ANetworkConnect::IsConnectToRoom() {
 	//マスターサーバーに接続されていないなら
-	if (UStrixBlueprintFunctionLibrary::IsMasterServerConnected(GetWorld()) == false) {
+	if (StrixUtil::IsMasterServerConnected(GetWorld()) == false) {
 		return false;
 	}
 
@@ -41,10 +43,11 @@ bool ANetworkConnect::IsConnectToRoom() {
 }
 
 void ANetworkConnect::InitializeMemberProperty(const FString name) {
-	UMyGameInstance::GetInstance()->GetNetworkData()->SetStringMemberProperty("name", name);
-	UMyGameInstance::GetInstance()->GetNetworkData()->SetBoolMemberProperty("IsStart", false);
-	UMyGameInstance::GetInstance()->GetNetworkData()->SetIntMemberProperty("PlayerIndex", 0);
-	UMyGameInstance::GetInstance()->GetNetworkData()->SetIntMemberProperty("BoatIndex", 0);
+	auto instance = UMyGameInstance::GetInstance();
+	instance->GetNetworkData()->SetStringMemberProperty("name", name);
+	instance->GetNetworkData()->SetBoolMemberProperty("IsStart", false);
+	instance->GetNetworkData()->SetIntMemberProperty("PlayerIndex", 0);
+	instance->GetNetworkData()->SetIntMemberProperty("BoatIndex", 0);
 }
 
 void ANetworkConnect::InitializeRoomProperty(const int32 capacity, const FString roomName) {
@@ -53,17 +56,53 @@ void ANetworkConnect::InitializeRoomProperty(const int32 capacity, const FString
 }
 
 void ANetworkConnect::UpdateMemberProperty(const int32 channelID) {
-	UMyGameInstance::GetInstance()->GetUserData()->SetChannelID(channelID);
-	FStrixRoomMember roomMember = UStrixBlueprintFunctionLibrary::GetCurrentRoomMember(GetWorld(), channelID);
-	UMyGameInstance::GetInstance()->GetUserData()->SetPlayerID(roomMember.Id);
-	
-	UMyGameInstance::GetInstance()->GetUserData()->SetPlayerName(*roomMember.Name);
+	auto instance = UMyGameInstance::GetInstance();
 
-	SetPlayerIndex(channelID);
+	//チャンネルIDをセット
+	instance->GetUserData()->SetChannelID(channelID);
+	FStrixRoomMember roomMember = StrixUtil::GetCurrentRoomMember(GetWorld(), channelID);
+	// IDと名前をセット
+	instance->GetUserData()->SetPlayerID(roomMember.Id);
+	instance->GetUserData()->SetPlayerName(*roomMember.Name);
+
+	SetPlayerIndex(StrixUtil::GetCurrentRoom(GetWorld(), channelID).MemberCount, channelID);
 	//ポーズ処理
-	UStrixBlueprintFunctionLibrary::PauseNetworkObjectManager(GetWorld(), channelID);
+	StrixUtil::PauseNetworkObjectManager(GetWorld(), channelID);
 }
 
-void ANetworkConnect::SetPlayerIndex(const int32 channelID) {
-	FStrixRoom roomInfo =  UStrixBlueprintFunctionLibrary::GetCurrentRoom(GetWorld(), channelID);
+void ANetworkConnect::SetPlayerIndex(const int32 memberCount, const int32 channelID) {
+	int32 count = memberCount;
+	int32 playerIndex = 0;
+	auto instance = UMyGameInstance::GetInstance();
+	//参加者が一人なら
+	if (count == 1) {
+		instance->GetUserData()->SetPlayerIndex(playerIndex);
+		instance->GetNetworkData()->SetIntMemberProperty("PlayerIndex", playerIndex);
+		return;
+	}
+
+	//自分のデータを取得
+	FStrixRoomMember myUserData = StrixUtil::GetCurrentRoomMember(GetWorld(), channelID);
+	//ルーム内のユーザー情報を取得
+	TArray<FStrixRoomMember> roomMembers = StrixUtil::GetCurrentRoomMembers(GetWorld(), channelID);
+	int32 memberIndex = 0;
+	bool result = false;
+	for (int i = 0; i < roomMembers.Num(); i++) {
+		//自分自身を調べようとしてるなら
+		if (myUserData.Id == roomMembers[i].Id) {
+			continue;
+		}
+		//相手のプレイヤー番号を取得する
+		StrixUtil::TryGetIntProperty(roomMembers[i].Properties, "PlayerIndex", result, memberIndex);
+		//失敗したかどうか
+		if (result == false) {
+			continue;
+		}
+		if (memberIndex <= playerIndex) {
+			playerIndex++;
+		}
+	}
+
+	instance->GetUserData()->SetPlayerIndex(playerIndex);
+	instance->GetNetworkData()->SetIntMemberProperty("PlayerIndex", playerIndex);
 }
