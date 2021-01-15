6995596fd28e6ab26dc46a9ee4dc5a8a8cbad66a
matsumoto <matsumoto.k017a1377@gmail.com>
2021-01-15 11:04:34 +0900

    ランキングのバグ直ったかもしれない

--- gff_08/Source/gff_08/utils/RankingSort.cpp
+++ gff_08/Source/gff_08/utils/RankingSort.cpp
@@ -6,20 +6,19 @@
 
 TArray<FGamePlayData> URankingSort::GetPlayDatasSortedByRanking() {
 	auto Data = UMyGameInstance::GetInstance()->GetPlayData();
-	auto GetSumTimes = [](const TArray<float>& Times) {
-		if (Times.Num() == 3) {
-			float Res = 0.0f;
-			for (auto&& Time : Times) {
-				// 0.0•b‚ªƒZƒbƒg‚³‚ê‚Ä‚¢‚½‚çŽü‰ñ’x‚ê
-				if (Time <= 0.01f) {
-					return FLT_MAX;
-				}
-				Res += Time;
+	auto GetSumTimes = [](const TArray<float>& Times) -> TTuple<int32, float> {
+		float Res = 0.0f;
+		for (int32 i = 0; i < 3; i++) {
+			if (!Times.IsValidIndex(i)) {
+				return TTuple<int32, float>{i, Res};
 			}
-			return Res;
-		} else {
-			return FLT_MAX;
+			if (Times[i] == 0.0f) {
+				return TTuple<int32, float>{i, Res};
+			}
+			Res += Times[i];
 		}
+
+		return TTuple<int32, float>{3, Res};
 	};
 
 	TArray<FGamePlayData> Res;
@@ -28,11 +27,25 @@ TArray<FGamePlayData> URankingSort::GetPlayDatasSortedByRanking() {
 	Res.Push(Data->Player3Data);
 	Res.Push(Data->Player4Data);
 	Res.Sort([GetSumTimes](const auto& A, const auto& B) {
-		const float SumLapTimes_A = GetSumTimes(A.LapTimes);
-		const float SumLapTimes_B = GetSumTimes(B.LapTimes);
-		return SumLapTimes_A < SumLapTimes_B;
+		const auto Res_A = GetSumTimes(A.LapTimes);
+		const int32 LapCount_A = Res_A.Get<0>();
+		const float SumLapTimes_A = Res_A.Get<1>();
+
+		const auto Res_B = GetSumTimes(B.LapTimes);
+		const int32 LapCount_B = Res_B.Get<0>();
+		const float SumLapTimes_B = Res_B.Get<1>();
+
+		if (LapCount_A == LapCount_B) {
+			return SumLapTimes_A < SumLapTimes_B;
+		} else {
+			return LapCount_A > LapCount_B;
+		}
 	});
 
+	for (auto&& R : Res) {
+		GEngine->AddOnScreenDebugMessage(-1, 30.0f, FColor::Green,
+			FString::Format(TEXT("{0}'s data ({1}, {2}, {3})"), {R.PlayerIndex, R.LapTimes[0], R.LapTimes[1], R.LapTimes[2]}));
+	}
 	return Res;
 }
 
